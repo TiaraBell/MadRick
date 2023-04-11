ea23fe486e1d2622dcc6de34feba71e603567863
Tiara Bell <tiara.bell2013@gmail.com>
2023-04-11 14:43:55 -0400

    Added RickandMortyAPI

---
+++ index.js
@@ -0,0 +1,23 @@
+/* 1. Take user input or randomize words for MadRick Puzzle
+ * 2. Once inputs are received, generate puzzle with inputs
+ * 3. Allow user to play again
+ *
+ * To Do:
+ * Come up with about 5 puzzles based on specific episodes.
+ * Link the API to show the characters, specific locations, and which
+ * episode each puzzle is from.
+ */
+
+// fetch("https://rickandmortyapi.com/api/character")
+//   .then((response) => response.json())
+//   .then((responseJson) => console.log(responseJson.results));
+const characters = document.getElementById("profile-pic");
+
+fetch("https://rickandmortyapi.com/api/character")
+  .then((response) => response.json())
+  .then((responseJson) => {
+    for (let { name, image } of responseJson.results) {
+      console.log(name);
+      console.log(image);
+    }
+  });
